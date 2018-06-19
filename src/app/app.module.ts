7c52f4012cce23c3fbabaa821370f8c1eeacbe70
Yooda <nieyuda@hotmail.com>
2018-06-19 11:24:37 +0900

    ver1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { PageContentComponent } from './page-content/page-content.component';
+import { MetricsGroupComponent } from './metrics-group/metrics-group.component';
+import { ProgressComponent } from './progress/progress.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    PageContentComponent,
+    MetricsGroupComponent,
+    ProgressComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule
